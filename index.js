6fa8ea05109cf46dffd560f6534ec8902a412e4d
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2023-12-03 12:18:31 -0700

    9 passing tests

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat() {
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+}
+
+function prependCat() {
+    const copyOfCats = ["Arnold", ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice(0, -1); {
+        return copyOfCats;
+    }
+    
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice (1); {
+        return copyOfCats;
+    }
+}
